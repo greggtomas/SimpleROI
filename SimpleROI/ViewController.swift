a8c7c033b39732bc0e0518d9c053498414eb1305
Gregg Tomas <javatomas@gmail.com>
2021-08-23 21:56:33 -0700

    Added IBOutlet and IBAction for the text fields and button, respectively

--- SimpleROI/ViewController.swift
+++ SimpleROI/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var soldPriceTextField: UITextField!
+    @IBOutlet weak var shippingFeeTextField: UITextField!
+    @IBOutlet weak var labelFeeTextField: UITextField!
+    @IBOutlet weak var otherFeesTextField: UITextField!
+    @IBOutlet weak var resultsDisplay: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculateButtonPressed(_ sender: UIButton) {
+    }
+    
+    
 }
 
